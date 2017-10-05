362275c9cc7b85c7305aa10bfb7eaa396f45b975
margatoch <32279907+margatoch@users.noreply.github.com>
2017-10-05 09:57:55 -0300

    Update enlazado.js

--- enlazado.js
+++ enlazado.js
@@ -2,18 +2,19 @@ var numbers=prompt("ingresar número de tarjeta"); /*coloque el prompt al princi
                                                   dentro de la función, pero no lo mostraba, entonces probé 
                                                   en la consola y lo cambie hacia afuera de la función
                                                   */
-
-function isValidCar(){
+isValidCard(numbers);//validar los datos que ingresa
+function isValidCard(){
 	
 	var result=[];
 	var result=numbers.reverse();//el reverse lo coloque porque sirve para invertir los elementos
 	//tngo que multiplicar cada casilla par(luhn)
 	for(var i=0; i<numbers.length; i++){ 
-	if(result==""){ // puse "", porque el usuario no puede ingresar un campo vacío
-	alert("tarjeta inválida");
-}else{
-	return alert("tarjeta válida");
+	    if(result==""){ // puse "", porque el usuario no puede ingresar un campo vacío
+	         alert("tarjeta inválida");
+    }   else{
+	      return alert("tarjeta válida");
 
 }
 }
 }
+
